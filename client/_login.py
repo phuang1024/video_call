8d35c8fd29ee6f42a8b8285902fcd9ca1ca1a845
Patrick Huang <huangpatrick16777216@gmail.com>
2021-01-09 13:50:40 -0800

    Draw buttons.

--- client/_login.py
+++ client/_login.py
@@ -22,7 +22,15 @@ from _elements import Button
 
 class Login:
     def __init__(self):
-        pass
+        self.status = "CHOOSE"
+        self.button_join = Button(FONT_MED.render("Join a meeting", 1, BLACK))
+        self.button_create = Button(FONT_MED.render("Create a meeting", 1, BLACK))
 
     def draw(self, window, events):
-        pass
+        width, height = window.get_size()
+
+        window.fill(WHITE)
+
+        if self.status == "CHOOSE":
+            self.button_join.draw(window, events, (width//2, height//2), (300, 50))
+            self.button_create.draw(window, events, (width//2, height//2+75), (300, 50))
