71b629d33f39eb3eaad5d15e0ff7f167c744a409
Patrick Huang <huangpatrick16777216@gmail.com>
2021-01-09 21:04:34 -0800

    Manager class.

---
+++ server/_manager.py
@@ -0,0 +1,35 @@
+#  ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+import string
+import random
+
+
+class Manager:
+    def __init__(self):
+        self.meetings = {}
+
+    def new_meeting(self):
+        get_key = lambda: "".join(random.choices(string.ascii_lowercase, k=8))
+        key = get_key()
+        while key in self.meetings:
+            key = get_key()
+
+        self.meetings[key] = Meeting()
+        return key
+
+
