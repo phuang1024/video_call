b20d47f3cb84fc4094ecfb34c1f3f1fc6dbb1779
Patrick Huang <huangpatrick16777216@gmail.com>
2021-01-09 16:23:35 -0800

    Added text input click.

--- client/_elements.py
+++ client/_elements.py
@@ -33,7 +33,8 @@ class Button:
         self.text = text
 
     def draw(self, window, events, loc, size):
-        loc = [loc[0]-size[0]//2, loc[1]]
+        loc[0] -= loc[0]-size[0]//2
+
         clicked = self.clicked(events, loc, size)
         color = (GRAY_DARK if clicked else GRAY_LIGHT) if self.hovered(loc, size) else WHITE
         text_loc = [loc[i] + (size[i]-self.text.get_size()[i])//2 for i in range(2)]
@@ -43,6 +44,48 @@ class Button:
         window.blit(self.text, text_loc)
 
         return clicked
+
+    def hovered(self, loc, size):
+        mouse = pygame.mouse.get_pos()
+        if loc[0] <= mouse[0] <= loc[0]+size[0] and loc[1] <= mouse[1] <= loc[1]+size[1]:
+            return True
+        return False
+
+    def clicked(self, events, loc, size):
+        if self.hovered(loc, size):
+            for event in events:
+                if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    return True
+        return False
+
+
+class TextInput:
+    def __init__(self, font):
+        self.font = font
+        self.cursor_pos = 0
+        self.text = ""
+        self.editing = False
+        self.frame = 0
+
+    def draw(self, window, events, loc, size):
+        loc = list(loc)
+        loc[0] -= loc[0]-size[0]//2
+
+        clicked = self.clicked(events, loc, size)
+        text = self.font.render(self.text, 1, BLACK)
+        text_loc = [loc[i] + (size[i]-text.get_size()[i])//2 for i in range(2)]
+        cursor_x = text_loc[0] + self.font.render(self.text[:self.cursor_pos], 1, BLACK).get_width()
+
+        color = GRAY_DARK if clicked else (GRAY_LIGHT if self.hovered(loc, size) and not self.editing else WHITE)
+        pygame.draw.rect(window, color, (*loc, *size))
+        pygame.draw.rect(window, BLACK, (*loc, *size), 2)
+        window.blit(text, text_loc)
+        if self.editing:
+            pygame.draw.line(window, BLACK, (cursor_x, loc[1]+10), (cursor_x, loc[1]+size[1]-10))
+
+        for event in events:
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                self.editing = self.hovered(loc, size)
 
     def hovered(self, loc, size):
         mouse = pygame.mouse.get_pos()
